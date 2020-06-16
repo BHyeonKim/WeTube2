d6b6f837b3cfea0db5eada7fe7d81cd6954ad224
BoHyunK <rlaqhguse@gmail.com>
2020-06-16 21:32:58 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const app = express()
 const PORT = 4000;
 
 const handleListening = () => {
-    console.log(`Listening on: http://localhost:PORT`)    
+    console.log(`Listening on: http://localhost:${PORT}`)    
 }
+
+function handleHome(req, res){
+    console.log(req)
+    res.send("Hi from Home")
+}
+
+function handleProfile(req,res){
+    res.send("You are on my profile")
+}
+
+app.get("/", handleHome)
+
+app.get("/profile", handleProfile)
+
 app.listen(PORT, handleListening)
